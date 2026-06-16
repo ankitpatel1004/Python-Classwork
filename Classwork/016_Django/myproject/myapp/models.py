c25a8861100712822616cb25f20539040a5f4e1c
Ankit <ankitpatel8085@gmail.com>
2026-06-16 20:14:37 +0530

    All

--- Classwork/016_Django/myproject/myapp/models.py
+++ Classwork/016_Django/myproject/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+class Product(models.Model):
+    p_name = models.CharField(max_length=50)
+    p_price = models.IntegerField()
+    p_quantity = models.IntegerField()
+    p_brand = models.CharField(max_length=50,default="Santoor")
+    p_description = models.CharField(max_length=100,null=True)
