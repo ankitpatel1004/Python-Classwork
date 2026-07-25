3fbc7b06ed5c222619c3d30709855c763eb0f267
Ankit <ankitpatel8085@gmail.com>
2026-07-25 21:38:07 +0530

    All

--- Classwork/028_DRF/myproject/myapp/urls.py
+++ Classwork/028_DRF/myproject/myapp/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path("create",create,name="create"),
     path("list",list,name="list"),
     path("update",update,name="update"),
-    path("delete",delete,name="delete")
+    path("delete",delete,name="delete"),
+    path("list-student",list_student,name="list-student"),
+    path("create-student",create_student,name="create-student"),
+    path("delete-student/<id>",delete_student,name="delete-student"),
+    path("update-student/<id>",update_student,name="update-student")
 ]
--- Classwork/028_DRF/myproject/myapp/views.py
+++ Classwork/028_DRF/myproject/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from myapp.models import *
+from myapp.serializer import *
 
 # Create your views here.
 @api_view(['POST'])
@@ -18,3 +20,34 @@ def update(request):
 @api_view(['DELETE'])
 def delete(request):
     return Response("DELETE API CALLING")
+
+@api_view(['GET'])
+def list_student(request):
+    students = Student.objects.all()
+    ser = StudentSerializer(students,many=True)
+    return Response({"data":ser.data})
+
+@api_view(['POST'])
+def create_student(request):
+    ser = StudentSerializer(data=request.data)
+    if not ser.is_valid():
+        return Response({"errors":ser.errors,"message":"Something went wrong"})
+    else:
+        ser.save()
+        return Response({"data":ser.data})
+
+@api_view(['DELETE'])
+def delete_student(request,id):
+    students = Student.objects.get(id=id)
+    students.delete()
+    return Response("Student deleted")
+
+@api_view(['PUT'])
+def update_student(request,id):
+    students = Student.objects.get(id=id)
+    ser = StudentSerializer(data=request.data)
+    if not ser.is_valid():
+        return Response({"errors":ser.errors,"message":"Something went wrong"})
+    else:
+        ser.save()
+        return Response({"data":ser.data})    
